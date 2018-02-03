d78a54681a76a78e86509f53f73cf8d50390ff37
ccjmne <ccjmne@gmail.com>
2018-02-03 17:49:31 +0100

    Pulled in more of RxJs

--- src/app/rxjs/rxjs.module.ts
+++ src/app/rxjs/rxjs.module.ts
@@ -1,7 +1,10 @@
-import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/merge';
+import 'rxjs/add/observable/of';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/mergeMapTo';
+import 'rxjs/add/operator/multicast';
+import 'rxjs/add/operator/startWith';
