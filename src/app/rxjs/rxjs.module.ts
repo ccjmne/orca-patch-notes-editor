256c726f9290b491a6fc455285239f9730acc17b
ccjmne <ccjmne@gmail.com>
2018-02-03 02:18:03 +0100

    Imported more of RxJs

--- src/app/rxjs/rxjs.module.ts
+++ src/app/rxjs/rxjs.module.ts
@@ -1,3 +1,7 @@
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/merge';
+
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/reduce';
