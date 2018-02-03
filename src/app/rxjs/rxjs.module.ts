a76a961467387e9acd9eab5c5fea8c4dc105510b
ccjmne <ccjmne@gmail.com>
2018-02-04 00:46:43 +0100

    Imported RxJs filter operator

--- src/app/rxjs/rxjs.module.ts
+++ src/app/rxjs/rxjs.module.ts
@@ -2,6 +2,7 @@ import 'rxjs/add/observable/merge';
 import 'rxjs/add/observable/of';
 
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/mergeMap';
