82a412d2eb0886a77d8446914e5e0d6cbda670ff
ccjmne <ccjmne@gmail.com>
2018-02-04 15:06:30 +0100

    Removing multicast RxJs operator in favor of share

--- src/app/rxjs/rxjs.module.ts
+++ src/app/rxjs/rxjs.module.ts
@@ -7,5 +7,5 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/mergeMapTo';
-import 'rxjs/add/operator/multicast';
+import 'rxjs/add/operator/share';
 import 'rxjs/add/operator/startWith';
